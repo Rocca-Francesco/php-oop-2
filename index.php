93ef77b708b8ff8d07a066026303625945592ba5
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-23 20:17:16 +0100

    checkpoint

--- index.php
+++ index.php
@@ -87,6 +87,59 @@ $cucciaCane = new Houses (
   "30x20x70"
 );
 
+$cucciaGatto = new Houses (
+  "Cuccia per gatti",
+  new Category (
+    "Gatto"
+  ),
+  92.40,
+  5,
+  "plastica, lana",
+  "30x30x90"
+);
+
+$polloFinto = new Games (
+  "Pollo finto con rumore",
+  new Category (
+    "Cane"
+  ),
+  7.99,
+  32,
+  "silicone"
+);
+
 var_dump($pallinaDiLana);
-var_dump($cucciaCane)
+var_dump($cucciaCane);
+var_dump($cucciaGatto);
+var_dump($polloFinto);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP Extends</title>
+
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="row row-cols-3">
+      <div class="col">
+          <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title"><?= ?></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
